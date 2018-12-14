d4d66b4b20c0656b446fe4dfb8ff5a9124028560
juan-ee <juanque@outlook.com>
2018-12-13 22:42:54 -0500

    refactor getRandomPositions function

--- utilities.js
+++ utilities.js
@@ -1,14 +1,15 @@
-function getRandomPositions(total, start, limit) {// TODO: change
-    var random = new Array(total);
-    var i = 0;
-    while (i < random.length) {
-        const num = Math.floor(Math.random() * limit + start);
-        if (!random.includes(num)) {
-            random[i] = num;
-            i++;
-        }
-    }
-    return random;
+const shuffle = require('shuffle-array');
+
+/**
+* @pickedNumbers - total number of elements to pick from the array
+* @maxNumber - max value that an element of the array can have
+* @startNumber - 0 or 1
+ *               if 0: an array of [0,...,maxNumber - 1] will be generated
+ *               if 1: an array of [1,...,maxNumber] will be generated
+ */
+function getRandomPositions(pickedNumbers, startNumber, maxNumber) {
+    const random_numbers = Array.from(Array(maxNumber), (_,x) => x+startNumber);
+    return shuffle.pick(random_numbers, { picks: pickedNumbers });
 }
 
 function toMili(minutes) {
--- utilities.spec.js
+++ utilities.spec.js
@@ -14,19 +14,37 @@ describe("Utilities Tests", () => {
         expect(toMinutes(mili)).to.be.equal(minutes);
         done();
     });
-    it('should generate a random array', () => {
-        const start = 1 , end = 5, total = 4;
-        const arr = getRandomPositions(total,start,end), all = [];
 
-        for(let i=start;i<=end;i++)
-            all.push(i);
+    it('should generate a random array, from 1 to 5', (done) => {
+        const picked = 5 , start = 1, max = 5;
+        const expected_elements = [1,2,3,4,5];
+        const array = getRandomPositions(picked, start, max);
 
-        expect(arr.length).to.eq(total);
-        console.log(arr);
-        arr.forEach((elem)=>{
-            if(!all.includes(elem))
-                console.log(elem,'in',all);
-            expect(all.includes(elem)).to.be.true;
-        })
+        expected_elements.forEach((expected_element)=>expect(array.includes(expected_element)).to.be.true);
+        done();
     });
+
+    it('should generate a random array, from 0 to 5', (done) => {
+        const picked = 6 , start = 0, max = 6;
+        const expected_elements = [0,1,2,3,4,5];
+        const array = getRandomPositions(picked, start, max);
+
+        expected_elements.forEach((expected_element)=>expect(array.includes(expected_element)).to.be.true);
+
+        done();
+    });
+
+    it('should generate a random array, from 0 to 9 - pick 2 elements', (done) => {
+        const picked = 2 , start = 0, max = 10;
+        const expected_elements = [0,1,2,3,4,5,6,7,8,9];
+        const array = getRandomPositions(picked, start, max);
+
+        console.log(array);
+        expect(array.length).to.be.eq(picked);
+        array.forEach((elem)=>expect(expected_elements.includes(elem)).to.be.true);
+
+        done();
+    });
+
+
 });
